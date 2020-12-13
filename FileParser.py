b8b449201505f9d4f6bcac6a233de3fdf950bbce
Bantosito <72502994+Bantosito@users.noreply.github.com>
2020-12-13 02:48:01 +0100

    comments for structuring for tomorrow

--- FileParser.py
+++ FileParser.py
@@ -92,25 +92,31 @@ def writeTurbine():
 #            pass
 #        else:
 #            #if turbineData[key].date_time()
+
+#1
 df = pd.read_csv(filenameTurbine,delimiter=";",decimal =",", parse_dates = ["Date/Time"])      
 #df.info()
+#2
 df1 = df["Theoretical_Power "].sub(df["Active_Power"]).to_frame("Power_Difference")
 dupa = {"FaultMsg" : "str"}
 df2 = pd.concat([df, df1], axis = 1).round(2)
+
+#changing datatype for #7
 df2["FaultMsg"] = df2["FaultMsg"].astype("object")
 
 
-
+#5
 df3 = df2.copy().sort_values(by=["Wind_Speed"], ascending = False).reset_index(drop=True)
 df3.info()
+#6
 df3.iloc[1:20].plot(x = "Wind_Speed", y = "Active_Power", rot = 0, kind = "bar",xlabel = "Wind speed (m/s)", ylabel = "Power(kW)" )
 plt.title("Wind speed against Active Power")
 plt.xticks(rotation = 45)
 #plt.show()
 
-
+#?
 print(df3["Wind_Speed"])
-
+#4
 index = ["Theoretical_Power ", "Active_Power"]
 
 afterDate = df["Date/Time"] >= "2018-05-01 04:00"
@@ -126,7 +132,7 @@ filtered.plot(x="Date/Time", y=index ,rot = 0, kind = "bar",xlabel = "Theoretica
 plt.title("Theoretical Power and Active Power")
 plt.xticks(rotation = 45)
 #plt.show()
-
+#7 taskOutput - can work as task #8
 taskEight = df2[df2["Error"] > 3]
 taskOutput = taskEight[taskEight["FaultMsg"] == True]
 
@@ -136,12 +142,12 @@ for row in taskOutput.index:
 
 #print(list)
 print(taskOutput)
-
+#9
 taskNine = taskOutput[taskOutput["Error"] > 50]
 print(taskNine)
 for row in taskNine.index:
     df2 = df2.drop([row])
-
+#3
 #printing back to the csv file
 df2.to_csv(turbineOutput, index =False)
     
